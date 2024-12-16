5848936f69bdf69d1af6451244b5dba67d9d6dd9
iskconproject <projectiskcon@gmail.com>
2024-12-16 06:45:58 +0530

    feat: add mahaprasadam form

--- app/(pages)/payment-failure/page.tsx
+++ app/(pages)/payment-failure/page.tsx
@@ -35,10 +35,42 @@ const PaymentFailurePage = () => {
   }, []);
 
   if (!paymentData) {
-    return <p>Loading...</p>;
+    return (
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+        <AlertTriangle className="h-16 w-16 text-red-500 dark:text-red-400" />
+        <h1 className="text-3xl font-bold mt-2">Payment Failed</h1>
+
+        <div className="pt-2 text-center text-sm">
+          <p className="text-gray-600">
+            Unfortunately, your payment could not be processed at this time.
+          </p>
+        </div>
+
+        <div className="mt-8">
+          <Link href="/">
+            <Button className="mr-4">Go to Home</Button>
+          </Link>
+          <Link href="/donation">
+            <Button variant="secondary" className="mr-4">
+              View Donations
+            </Button>
+          </Link>
+          <Link href="/contact-us">
+            <Button variant="secondary">Contact Us</Button>
+          </Link>
+        </div>
+      </main>
+    );
   }
 
-  const { uniqueRefNumber, responseCode, totalAmount, transactionAmount, paymentMode, id } = paymentData;
+  const {
+    uniqueRefNumber,
+    responseCode,
+    totalAmount,
+    transactionAmount,
+    paymentMode,
+    id,
+  } = paymentData;
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -99,16 +131,16 @@ const PaymentFailurePage = () => {
               <p className="font-semibold text-yellow-700">Important Notice</p>
             </div>
             <p className="text-sm text-yellow-800">
-              If the amount has been deducted from your bank account, please save a
-              screenshot of the transaction for reference and contact us
+              If the amount has been deducted from your bank account, please
+              save a screenshot of the transaction for reference and contact us
               immediately. Our team will assist you in resolving this issue.
             </p>
           </div>
         </CardContent>
         <blockquote className="text-gray-500 italic text-sm mb-8 p-4">
-          "When one is in difficulty, the consciousness of God comes naturally. A
-          sincere devotee prays to the Lord, whether in happiness or distress." -
-          Srila Prabhupada
+          "When one is in difficulty, the consciousness of God comes naturally.
+          A sincere devotee prays to the Lord, whether in happiness or
+          distress." - Srila Prabhupada
         </blockquote>
       </Card>
 
